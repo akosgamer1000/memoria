61d9061b66122f68ad0cc057fc876d58274b15e2
Petrik Diak <diak@petrik.hu>
2024-03-05 12:04:46 +0100

    Add project files.

---
+++ memoria/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace memoria
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public static int válsztott;
+		public MainWindow()
+		{
+			InitializeComponent();
+			alap();
+		}
+		private void alap()
+		{
+			cbox.Items.Add(2);
+			cbox.Items.Add(4);
+			cbox.Items.Add(6);
+			cbox.Items.Add(8);
+		}
+		private void change(object sender, SelectionChangedEventArgs e)
+		{
+			válsztott = int.Parse(  cbox.SelectedItem.ToString());
+			játék j=new játék();
+			j.Show();
+			this.Hide();
+		}
+	}
+}
---
+++ memoria/játék.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace memoria
+{
+	/// <summary>
+	/// Interaction logic for játék.xaml
+	/// </summary>
+	public partial class játék : Window
+	{
+
+		int s = MainWindow.válsztott;
+		List<Button> buttons = new List<Button>();
+		List<Image> images = new List<Image>();
+		
+		public játék()
+		{
+			InitializeComponent();
+			greadgeneration();
+			
+		}
+		public void greadgeneration()
+		{
+			for(int i = 0; i < s; i++)
+			{
+				racs.ColumnDefinitions.Add(new ColumnDefinition());
+				
+				
+				racs.RowDefinitions.Add(new RowDefinition());
+				
+			}
+			int x = 1;
+			for(int i = 0;i < s; i++)
+			{
+				for(int j = 0; j < s; j++)
+				{
+					Button b=new Button();
+					//b.Content = new BitmapImage(new Uri("letöltés.jpg",UriKind.RelativeOrAbsolute));
+					Image img = new Image();
+					img.Stretch = Stretch.Fill;
+					img.Source=new BitmapImage(new Uri("letöltés.jpg", UriKind.RelativeOrAbsolute));
+					b.Content = img;
+
+					x++;
+					Grid.SetColumn(b, j);
+					Grid.SetRow(b, i);
+					racs.Children.Add(b);
+				}
+			}
+			racs.ShowGridLines = true;
+		}
+		public void imageload()
+		{
+
+			for( int i = 0; i < s;i++)
+			{
+				images.Add(new BitmapImage(new Uri(""));
+
+			}
+		}
+	}
+}
